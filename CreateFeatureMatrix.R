b2c73796bfa7186e6e3d1aa85884e05ad8ef88e8
Badi James <badi.t.james@gmail.com>
2018-09-01 00:01:01 +1200

    Separated the feature matrix creating code into it's own file
    Redesigned to take in any miRNA sequence and target/nontarget set

---
+++ CreateFeatureMatrix.R
@@ -0,0 +1,380 @@
+library(rentrez)
+library(Biostrings)
+library(dequer)
+
+
+getmRNASequence <- function(accNo){
+  # Querry nucleotide
+  # Parse the sequences out of the FASTA
+  # Parse the codon start and stop out of the feature table
+  fasta <- entrez_fetch("nucleotide", id=accNo, rettype = "fasta")
+  seqLines <- unlist(strsplit(fasta, "\n"))
+  seqString <- paste0(seqLines[2:(length(seqLines)-1)], collapse = "")
+  featTable <- entrez_fetch("nucleotide", id=accNo, rettype = "ft")
+  cdsStartStop <- regmatches(featTable, regexpr("\\d+\\t\\d+\\tCDS", featTable))
+  cdsStartStop <- unlist(strsplit(cdsStartStop, "\t"))
+  cdsStart <- as.integer(cdsStartStop[1])
+  cdsStop <- as.integer(cdsStartStop[2])
+  output <- list(accNo, seqString, cdsStart, cdsStop)
+  names(output) <- c("GenBank Accession", "Full Sequence", "CDS Start", "CDS Stop")
+  return(output)
+}
+
+
+guWob_seqs <- function(seq){
+  # Outputs a DNAStringSet that represent every possible site that
+  # involves at least one GU complement (with everywhere else perfectly 
+  # WC complementing).
+  # seq: sequence of the perfect WC complement. The GU complements are bulit
+  # from this
+  output <- c()
+  seqQ <- queue()
+  pushback(seqQ, list(seq, 1))
+  while(length(seqQ) > 0){
+    sTup <- pop(seqQ)
+    sCurrent <- sTup[[1]]
+    startPos <- sTup[[2]]
+    for(i in c(startPos:length(sCurrent))){
+      if(letter(sCurrent, i) == "C"){
+        snew <- replaceAt(sCurrent, IRanges(i,i), "T")
+        if(i < length(sCurrent)) pushback(seqQ, list(snew, i+1))
+        output <- c(output, snew)
+      }
+      if(letter(sCurrent, i) == "A"){
+        snew <- replaceAt(sCurrent, IRanges(i,i), "G")
+        if(i < length(sCurrent)) pushback(seqQ, list(snew, i+1))
+        output <- c(output, snew)
+      }
+    }
+  }
+  return(DNAStringSet(output))
+}
+
+
+misMatchNotGU <- function(seq, start = 1, end = NULL, reference = NULL){
+  # Outputs a DNAStringSet representing every site with all bp wc
+  # complementing one of the DNAStrings in seq except for one mismatch
+  # between positions start and end.
+  # The introduced mismatch does not allow for nucleotides that would 
+  # make the position a GU complement (as that is considered a different
+  # site class).
+  # If seq represents sites with GU complements, reference should be a 
+  # DNAString of the perfect wc complement. This is used to ensure that
+  # 1: for DNAString in seq with only one GU complement the mismatch is
+  #    not put in the same place
+  # 2: for DNAString in seq with only multiple GU complements the mismatch 
+  #    at a GU complement doesn't allow for a wc complement there 
+  if(class(seq) != "DNAStringSet"){
+    seq <- DNAStringSet(seq) # allows inputting a single DNAString
+  }
+  output <- c()
+  
+  # IUPAC letter for "any nucleotide that wont make this a wc or gu pair"
+  mismatchMap <- c("Y", "R", "H", "V")
+  names(mismatchMap) <- c("A", "C", "G", "T")
+  
+  # for when seq is of sites with GU pairing
+  noGUWC <- c("Y", "R")
+  names(noGUWC) <- c("G", "T")
+  
+  theEndWasNull <- is.null(end) 
+  for(j in 1:length(seq)){ 
+    scurrent <- seq[[j]]
+    if(theEndWasNull) end <- length(scurrent)
+    badi <- -1 # site of only GU pair
+    alreadyGU <- c()
+    if(!is.null(reference)){
+      # find the gu pairs
+      scomp <- compareStrings(scurrent, reference)
+      diffs <- gregexpr("\\?", scomp)[[1]]
+      if(length(diffs) == 1) badi <- diffs[1]
+      else if(length(diffs) > 1) alreadyGU <- diffs
+    }
+    for(i in start:end){
+      if(i != badi){
+        if(i %in% alreadyGU){
+          snew <- replaceAt(scurrent, IRanges(i,i), 
+                            noGUWC[letter(scurrent, i)])
+        } else {
+          snew <- replaceAt(scurrent, IRanges(i,i), 
+                            mismatchMap[letter(scurrent, i)])
+        }
+        output <- c(output, snew)
+      }
+    }
+  }
+  return(unique(DNAStringSet(output)))
+}
+
+
+sitesOfClass <- function(classSearch, nameOfClass, rnaSet, rnaCDS, 
+                         centeredClass = NULL) 
+{
+  if(class(classSearch) != "DNAStringSet"){
+    classSearch <- DNAStringSet(classSearch) # allows inputting a single DNAString
+  }
+  classMatches <- sapply(classSearch, vmatchPattern, subject = rnaSet,
+                         fixed = "subject")
+  classEnds <- sapply(classMatches, endIndex)
+  classCounts <- sapply(classMatches, elementNROWS)
+  allClassEnds <- vector('list', length(rnaSet))
+  allClassCounts <- rep(0, length(rnaSet))
+  meanDistFrom3 <- rep(NaN, length(rnaSet))
+  varDistFrom3 <- rep(NaN, length(rnaSet))
+  medianDistFrom3 <- rep(NaN, length(rnaSet))
+  meanDistFromStop <- rep(NaN, length(rnaSet))
+  varDistFromStop <- rep(NaN, length(rnaSet))
+  medianDistFromStop <- rep(NaN, length(rnaSet))
+  freqIn3 <- rep(NaN, length(rnaSet))
+  freqInCDS <- rep(NaN, length(rnaSet))
+  freqIn5 <- rep(NaN, length(rnaSet))
+  
+  if(!is.null(centeredClass)){
+    freqStart3 <- rep(NaN, length(rnaSet))
+    freqStart4 <- rep(NaN, length(rnaSet))
+    freqStart5 <- rep(NaN, length(rnaSet))
+  }
+  
+  # for counting the nucleotide frequencies in and around the sites
+  siteIndex <- seq(36, -10, -1)
+  siteIndex <- siteIndex[siteIndex != 0]
+  nucFreqMat <- matrix(NaN, nrow = length(rnaSet), ncol = 46*4)
+  
+  for(i in 1:length(rnaSet)){
+    classEnd <- c()
+    classCount <- 0
+    for(j in 1:length(classMatches)){
+      if(!is.null(classEnds[[i, j]])) classEnd <- c(classEnd, classEnds[[i, j]])
+      classCount <- classCount + classCounts[i,j]
+    }
+    if(classCount > 0) {
+      cdsStart <- rnaCDS$`CDS Start`[[i]]
+      cdsStop <- rnaCDS$`CDS Stop`[[i]]
+      allClassCounts[i] <- classCount
+      allClassEnds[[i]] <- classEnd
+      distFrom3 <- length(rnaSet[[i]]) - classEnd
+      meanDistFrom3[i] <- mean(distFrom3)
+      varDistFrom3[i] <- var(distFrom3)
+      medianDistFrom3[i] <- median(distFrom3)
+      
+      siteAreaSeqs <- DNAStringSet(sapply(
+        classEnd, function(x) 
+          subseq(rnaSet[[i]], start = x - 35, end = x + 10)))
+      nucFreqs <- c(sapply(1:46, function(x) 
+        nucleotideFrequencyAt(siteAreaSeqs, c(x), as.prob = TRUE)))
+      nucFreqMat[i,] <- nucFreqs
+      
+      if(length(cdsStop) > 0){
+        distFromStop <- cdsStop - classEnd
+        meanDistFromStop[i] <- mean(distFromStop)
+        varDistFromStop[i] <- var(distFromStop)
+        medianDistFromStop[i] <- median(distFromStop)
+        freqIn5[i] <- length(which(classEnd < cdsStart)) / length(classEnd)
+        freqInCDS[i] <- length(which(classEnd >= cdsStart 
+                                     & classEnd <= cdsStop)) / length(classEnd)
+        freqIn3[i] <- length(which(classEnd > cdsStop)) / length(classEnd)
+      } else { # Non coding RNA
+        meanDistFromStop[i] <- NA
+        varDistFromStop[i] <- NA
+        medianDistFromStop[i] <- NA
+        freqIn5[i] <- NA
+        freqInCDS[i] <- NA
+        freqIn3[i] <- NA
+      }
+      
+      if(!is.null(centeredClass)){
+        freqStart3[i] <- classCounts[i, which(centeredClass == 3)] / classCount
+        freqStart4[i] <- classCounts[i, which(centeredClass == 4)] / classCount
+        freqStart5[i] <- classCounts[i, which(centeredClass == 5)] / classCount
+      }
+    } 
+  }
+  
+  colnames(nucFreqMat) <- c(sapply(
+    siteIndex, function(x) 
+      paste0(nameOfClass, "_freq", c("A", "C", "G", "T"), x)))
+  
+  outputDF <- as.data.frame(
+    cbind(allClassCounts, meanDistFrom3, medianDistFrom3, varDistFrom3,
+          meanDistFromStop, medianDistFromStop, varDistFromStop, freqIn5, 
+          freqInCDS, freqIn3))
+  colnames(outputDF) <- c(paste0("n", nameOfClass), paste0(nameOfClass, "_",
+                                                           c("end3'distMean", "end3'distMedian", "end3'distVar", 
+                                                             "stopdistMean", "stopdistMedian", "stopdistVar", 
+                                                             "5'UTRFreq", "CDSFreq", "3'UTRFreq")))
+  outputDF <- cbind(outputDF, nucFreqMat)
+  
+  if(!is.null(centeredClass)){
+    cenStart <- cbind(freqStart3, freqStart4, freqStart5)
+    colnames(cenStart) <- paste0(nameOfClass, "_", "freqStart", c(3,4,5))
+    outputDF <- cbind(outputDF, cenStart)
+  }
+  
+  return(list(allClassEnds, outputDF))
+}
+
+# TODO: Change to make target or not column optional
+trainAndTestFeatureMatrix <- function(miRNAString, accType)
+{
+  # Fills a feature matrix for training and testing the Random Forest
+  # miRNAString is the sequence of the mature microRNA
+  # accType is a two collumn data frame/matrix. 
+  #    - first col is the GenBank accession numbers for the RNA
+  #    - Second collumn indicates if it's a target or not
+  
+  if(class(miRNAString) %in% c("DNAString", "RNAString")){
+    miRNAString <- as.character(miRNAString)
+  }
+  # Converted to DNAString to match the mRNA sequence type
+  # Also because i don't know if wildcard nucleotides work when 
+  # using matchPattern with RNAString
+  miRNAString <- gsub("U", "T", miRNAString)
+  
+  miLen <- nchar(miRNAString)
+  
+  # Create feature values for nucleotide identies of miRNA
+  # Assumes max length of miRNA is 26 and deals with varying length of
+  # miRNA by using character "X" to represent missing nucleotides
+  miNucIdenVec <- strsplit(miRNAString, split = "")[[1]]
+  miNucIdenFeats <- rep("X", 26)
+  miNucIdenFeats[1:length(miNucIdenVec)] <- miNucIdenVec
+  
+  miRNAString <- DNAString(miRNAString)
+  
+  # Query Nucleotide using the accession numbers to get a data frame of
+  # sequence strings and, for the mRNAs that are protein coding, the CDS
+  # start and stop sites
+  seqAndCDS <- as.data.frame(t(sapply(accType[,1], getmRNASequence)))
+  mRNAStrings <- DNAStringSet(seqAndCDS$`Full Sequence`)
+  
+  # Create search strings for the different classes of sites
+  # =========================================
+  
+  miRNA_revComp <- reverseComplement(miRNAString)
+  # Basic seed
+  seq_6mer <- subseq(miRNA_revComp, start = (length(miRNA_revComp)-6), 
+                     end = (length(miRNA_revComp)-1))
+  
+  # Preventing overlap in searches
+  # get identities of nucleotides at position 1 and 8 of site
+  iden8 <- letter(miRNA_revComp, (length(miRNA_revComp)-7))
+  iden1 <- letter(miRNA_revComp, (length(miRNA_revComp)))
+  # The IUPAC letter for "any nucleotide type except one"
+  notMatches <- c("V", "H", "D", "B")
+  names(notMatches) <- c("T", "G", "C", "A")
+  # The IUPAC letter for "any nucleotide type except A and one other"
+  notMatchNotA <- c("S", "Y", "K", "B")
+  names(notMatchNotA) <- c("T", "G", "C", "A")
+  notMatch8 <- notMatches[iden8]
+  notMatch1 <- notMatches[iden1]
+  notMorA1 <- notMatchNotA[iden1]
+  
+  # Class1 search strings
+  seq_7merm8 <- xscat(subseq(miRNA_revComp, start = (length(miRNA_revComp)-7), 
+                             end = (length(miRNA_revComp)-1)), notMatch1)
+  seq_8mer <- subseq(miRNA_revComp, start = (length(miRNA_revComp)-7), 
+                     end = length(miRNA_revComp))
+  seq_7merA1 <- xscat(notMatch8, subseq(miRNA_revComp, start = (length(miRNA_revComp)-6), 
+                                        end = (length(miRNA_revComp)-1)), "A")
+  class1Search <- DNAStringSet(list(seq_8mer, seq_7merm8, seq_7merA1))
+  
+  # Class 2 search strings
+  # GU wobble not allowed at position 1 or 8 so concatenate the seach letters
+  # for 1 and 8 to GU wobble versions of the core 2-7 seed
+  c2GU <- guWob_seqs(seq_6mer)
+  c2GU7merm8 <- xscat(letter(miRNA_revComp, length(miRNA_revComp)-7), c2GU, 
+                      notMatch1)
+  c2GU8mer <- xscat(letter(miRNA_revComp, length(miRNA_revComp)-7), c2GU,
+                    letter(miRNA_revComp, length(miRNA_revComp)))
+  c2GU7merA1 <- xscat(notMatch8, c2GU, "A")
+  c2Full <- DNAStringSet(c(c2GU7merA1, c2GU7merm8, c2GU8mer))
+  
+  # class 3 search strings
+  class3search <- misMatchNotGU(class1Search, start = 2, end = 7)
+  
+  # class 4 search strings
+  c4A1 <- misMatchNotGU(c2GU7merA1, start = 2, end = 7, 
+                        reference = seq_7merA1)
+  c4m8 <- misMatchNotGU(c2GU7merm8, start = 2, end = 7, 
+                        reference = seq_7merm8)
+  c4_8mer <- misMatchNotGU(c2GU8mer, start = 2, end = 7, 
+                           reference = seq_8mer)
+  class4search <- DNAStringSet(c(c4A1, c4m8, c4_8mer))
+  
+  # class 5 search string
+  class5Search <- xscat(notMatch8, seq_6mer, notMorA1)
+  
+  # class 6 search strings
+  cent3 <- subseq(miRNA_revComp, start = (length(miRNA_revComp)-12), 
+                  end = (length(miRNA_revComp)-2))
+  cent4 <- subseq(miRNA_revComp, start = (length(miRNA_revComp)-13), 
+                  end = (length(miRNA_revComp)-3))
+  cent5 <- subseq(miRNA_revComp, start = (length(miRNA_revComp)-14), 
+                  end = (length(miRNA_revComp)-4))
+  class6Search <- DNAStringSet(list(cent3, cent4, cent5))
+  c6SearchCenStart <- c(3,4,5)
+  
+  # class 7 search strings
+  GUcent3 <- guWob_seqs(cent3)
+  GUcent4 <- guWob_seqs(cent4)
+  GUcent5 <- guWob_seqs(cent5)
+  class7Search <- DNAStringSet(c(GUcent3, GUcent4, GUcent5))
+  c7SearchCenStart <- c(rep(3, length(GUcent3)),rep(4, length(GUcent4)),
+                        rep(5, length(GUcent5)))
+  
+  # class 8 search strings
+  mm_cent3 <- misMatchNotGU(cent3)
+  mm_cent4 <- misMatchNotGU(cent4)
+  mm_cent5 <- misMatchNotGU(cent5)
+  class8Search <- DNAStringSet(c(mm_cent3, mm_cent4, mm_cent5))
+  c8SearchCenStart <- c(rep(3, length(mm_cent3)),rep(4, length(mm_cent4)),
+                        rep(5, length(mm_cent5)))
+  
+  # class 9 search strings
+  c9cent3 <- misMatchNotGU(GUcent3, reference = cent3)
+  c9cent4 <- misMatchNotGU(GUcent4, reference = cent4)
+  c9cent5 <- misMatchNotGU(GUcent5, reference = cent5)
+  class9Search <- DNAStringSet(c(c9cent3, c9cent4, c9cent5))
+  c9SearchCenStart <- c(rep(3, length(c9cent3)),rep(4, length(c9cent4)),
+                        rep(5, length(c9cent5)))
+  
+  # Find sites and measure values for site features 
+  sitesClass1 <- sitesOfClass(class1Search, "c1", mRNAStrings, seqAndCDS)
+  sitesClass2 <- sitesOfClass(c2Full, "c2", mRNAStrings, seqAndCDS) 
+  sitesClass3 <- sitesOfClass(class3search, "c3", mRNAStrings, seqAndCDS) 
+  sitesClass4 <- sitesOfClass(class4search, "c4", mRNAStrings, seqAndCDS) 
+  sitesClass5 <- sitesOfClass(class5Search, "c5", mRNAStrings, seqAndCDS) 
+  sitesClass6 <- sitesOfClass(class6Search, "c6", mRNAStrings, 
+                              seqAndCDS, c6SearchCenStart) 
+  sitesClass7 <- sitesOfClass(class7Search, "c7", mRNAStrings, 
+                              seqAndCDS, c7SearchCenStart) 
+  sitesClass8 <- sitesOfClass(class8Search, "c8", mRNAStrings, 
+                              seqAndCDS, c8SearchCenStart) 
+  sitesClass9 <- sitesOfClass(class9Search, "c9", mRNAStrings, 
+                              seqAndCDS, c9SearchCenStart) 
+  
+  # Combine the tables
+  fullFeatTable <- cbind(sitesClass1[[2]], sitesClass2[[2]],
+                         sitesClass3[[2]], sitesClass4[[2]],
+                         sitesClass5[[2]], sitesClass6[[2]],
+                         sitesClass7[[2]], sitesClass8[[2]],
+                         sitesClass9[[2]])
+  
+  # Add collumn for total sites found accross all classes
+  fullFeatTable$N <- rowSums(fullFeatTable[,paste0("nc", 1:9)])
+  
+  # Add collumns for identities of miRNA sequence
+  miNucIdenFeatsMat <- t(matrix(miNucIdenFeats, nrow = nrow(accType),
+                                ncol = length(miNucIdenFeats)))
+  colnames(miNucIdenFeats) <- paste0("m1", 1:26)
+  fullFeatTable <- cbind(fullFeatTable, miNucIdenFeatsMat)
+  
+  # Add collumns for length of miRNA and mRNA
+  fullFeatTable$miLen <- miLen
+  fullFeatTable$mRNALen <- sapply(seqAndCDS$`Full Sequence`, nchar)
+  
+  # So we can supervise learning
+  fullFeatTable$Target <- accType[,2]
+  
+  return(fullFeatTable)
+}
